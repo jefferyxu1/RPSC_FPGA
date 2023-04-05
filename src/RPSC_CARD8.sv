ce278004b888baa96f38b7879f41ddc7a49845b8
jefferyxu1 <46063838+jefferyxu1@users.noreply.github.com>
2023-04-04 17:22:46 -0700

    Update to Card 9

--- src/RPSC_CARD8.sv
+++ src/RPSC_CARD8.sv
@@ -6,7 +6,8 @@ module RPSC_CARD8(clk, reset,
                     i43_FF13_G2_PS_Local_IN,
                     i57_FF14_I_G2_High_IN,
                     i76_FF16_FAN_OFF_Delay_IN,
-                    o6_FF9_HV_Connector_OUT, o3_FF9_HV_Connector_LA, o9_FF9_HV_Connector_PAMP_Interlock,
+                    o6_FF9_HV_Connector_OUT, o3_FF9_HV_Connector_LA,
+                    o19_FF10_G1_PS_Local_LA,
                     o22_FF11_Anode_PS_Local_LA,
                     o39_FF12_Anode_PS_Dummy_LA,
                     o42_FF13_G2_PS_Local_LA,
@@ -22,7 +23,7 @@ module RPSC_CARD8(clk, reset,
                 i57_FF14_I_G2_High_IN,
                 i76_FF16_FAN_OFF_Delay_IN;
     
-    output logic o6_FF9_HV_Connector_OUT, o3_FF9_HV_Connector_LA, o9_FF9_HV_Connector_PAMP_Interlock, // EP8 is gone, but still goes to Card7-66
+    output logic o6_FF9_HV_Connector_OUT, o3_FF9_HV_Connector_LA, // EP8 is gone, but still goes to Card7-66
                  o19_FF10_G1_PS_Local_LA, 
                  o22_FF11_Anode_PS_Local_LA,
                  o39_FF12_Anode_PS_Dummy_LA,
