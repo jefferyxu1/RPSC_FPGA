de78dd560a2403814f2674bea232245ba1ace53c
jefferyxu1 <46063838+jefferyxu1@users.noreply.github.com>
2023-04-14 16:00:17 -0700

    Add Flip Flop Design

---
+++ src/FF.sv
@@ -0,0 +1,14 @@
+module FF (out, LA, in, reset, LA_Test, clk);
+    input logic in, reset, LA_Test, clk;
+    output logic out, LA;
+
+    assign LA = out | LA_Test;
+
+    always_ff (posedge clk) begin
+        if (reset)
+            out <= 1'b0;
+        else
+            out <= ~in;
+    end
+
+endmodule
