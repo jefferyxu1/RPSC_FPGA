53de9b8d9f27618929852f153527ef29bddace26
jefferyxu1 <46063838+jefferyxu1@users.noreply.github.com>
2023-04-14 01:08:14 -0700

    Card 5, 18 Testbench

--- src/RPSC_CARD18.sv
+++ src/RPSC_CARD18.sv
@@ -1,5 +1,5 @@
 `timescale 1ns/10ps
-// TODO: Figure out Input pin 17 and 34
+// TODO: Figure out Input pin 12, 17, and 34
 
 module RPSC_CARD18 (i3_Not_DR_AMP_ON, i4_Not_FAN_ON, i5_Not_G1_ON, i6_Not_CA_ON, i34_Not_ANY_HV_GO_OFF,
 					i17_PS_ON, i18_Not_G2_ON, i19_Not_Anode_ON, i77_Not_RF_RED, i75_Not_RF_PERM, i73_Not_AN_HV_Ready,
@@ -9,29 +9,11 @@ module RPSC_CARD18 (i3_Not_DR_AMP_ON, i4_Not_FAN_ON, i5_Not_G1_ON, i6_Not_CA_ON,
 	input  logic i17_PS_ON, i18_Not_G2_ON, i19_Not_Anode_ON, i77_Not_RF_RED, i75_Not_RF_PERM, i73_Not_AN_HV_Ready;
 	output logic o47_Not_SB_ON, o42_Not_SB_OFF, o46_Not_HV_ON, o45_Not_HV_ON, o78_Not_RF_PERM, o76_Not_RF_PERM, o74_Not_HV_Ready;
 	
-	logic or_o1; 
-	
-	// Circuit 1 
-	or   #0.05 
-	or1   
-	(o47_Not_SB_ON,      
-	 i4_Not_FAN_ON,    
-	 i5_Not_G1_ON,         
-	 i6_Not_CA_ON,);
-	
-	not  #0.05 
-	inv1  
-	(o42_Not_SB_OFF,  
-	 o47_Not_SB_ON);
-	
-	// Circuit 2
-	or   #0.05 
-	or2   
-	 (o46_Not_HV_ON,      
-	  i18_Not_G2_ON,    
-	  i19_Not_Anode_ON);
+	assign o47_Not_SB_ON = i4_Not_FAN_ON | i5_Not_G1_ON | i6_Not_CA_ON;
+	assign o42_Not_SB_OFF = ~(i3_Not_DR_AMP_ON & i4_Not_FAN_ON & i5_Not_G1_ON & i6_Not_CA_ON);
+	assign o46_Not_HV_ON = i18_Not_G2_ON | i19_Not_Anode_ON;
 		 
-	nANd   #0.05 
+	nand   #0.05 
 	nANd1   
 	  (o45_Not_HV_ON,      
 	   i18_Not_G2_ON,    
@@ -46,32 +28,27 @@ endmodule
 
 module RPSC_CARD18_testbench(); 
   
-	logic i4_Not_FAN_ON, i5_Not_G1_ON, i6_Not_CA_ON;
-	logic i18_Not_G2_ON, i19_Not_Anode_ON, i77_Not_RF_RED, i75_Not_RF_PERM, i73_Not_AN_HV_Ready;
+	logic i3_Not_DR_AMP_ON, i4_Not_FAN_ON, i5_Not_G1_ON, i6_Not_CA_ON, i34_Not_ANY_HV_GO_OFF;
+	logic i17_PS_ON, i18_Not_G2_ON, i19_Not_Anode_ON, i77_Not_RF_RED, i75_Not_RF_PERM, i73_Not_AN_HV_Ready;
 	logic o47_Not_SB_ON, o42_Not_SB_OFF, o46_Not_HV_ON, o45_Not_HV_ON, o78_Not_RF_PERM, o76_Not_RF_PERM, o74_Not_HV_Ready;
   
-    RPSC_CARD18 dut (i4_Not_FAN_ON, i5_Not_G1_ON, i6_Not_CA_ON,
-		i18_Not_G2_ON, i19_Not_Anode_ON, i77_Not_RF_RED, i75_Not_RF_PERM, i73_Not_AN_HV_Ready,
-		o47_Not_SB_ON, o42_Not_SB_OFF, o46_Not_HV_ON, o45_Not_HV_ON, o78_Not_RF_PERM, o76_Not_RF_PERM, o74_Not_HV_Ready) ; 
-			
-	initial begin   
-   
-    i4_Not_FAN_ON=0; i5_Not_G1_ON  =0; i6_Not_CA_ON     =0; 
-	 i18_Not_G2_ON  =0; i19_Not_Anode_ON=0; 
-	 i77_Not_RF_RED =0; i75_Not_RF_PERM =0; i73_Not_AN_HV_Ready=0; #10;
-	 i4_Not_FAN_ON=0; i5_Not_G1_ON  =0; i6_Not_CA_ON     =1; 
-	 i18_Not_G2_ON  =0; i19_Not_Anode_ON=1; 
-	 i77_Not_RF_RED =0; i75_Not_RF_PERM =0; i73_Not_AN_HV_Ready=1; #10;
-	 i4_Not_FAN_ON=0; i5_Not_G1_ON  =1; i6_Not_CA_ON     =0; 
-	 i18_Not_G2_ON  =1; i19_Not_Anode_ON=0; 
-	 i77_Not_RF_RED =0; i75_Not_RF_PERM =1; i73_Not_AN_HV_Ready=0; #10;
-	 i4_Not_FAN_ON=1; i5_Not_G1_ON  =0; i6_Not_CA_ON     =0; 
-	 i18_Not_G2_ON  =1; i19_Not_Anode_ON=0; 
-	 i77_Not_RF_RED =1; i75_Not_RF_PERM =0; i73_Not_AN_HV_Ready=0; #10;
-	 i4_Not_FAN_ON=1; i5_Not_G1_ON  =1; i6_Not_CA_ON     =1; 
-	 i18_Not_G2_ON  =1; i19_Not_Anode_ON=1; 
-	 i77_Not_RF_RED =1; i75_Not_RF_PERM =1; i73_Not_AN_HV_Ready=1; #10;
+    RPSC_CARD18 dut (.*) ; 
+
+	initial begin 
+		{i73_Not_AN_HV_Ready, i75_Not_RF_PERM, i77_Not_RF_RED} = 3'b000;
+
+		for (int j = 0; j < 16; j++) begin
+			{i3_Not_DR_AMP_ON, i4_Not_FAN_ON, i5_Not_G1_ON, i6_Not_CA_ON} = j;
+			#1000;
+		end
+
+		for (int j = 0; j < 8; j++) begin
+			{i18_Not_G2_ON, i19_Not_Anode_ON, i34_Not_ANY_HV_GO_OFF} = j;
+			#1000;
+		end
+
 	 
-  end   		
+	end   		
   
 endmodule 
+
--- src/RPSC_CARD5.sv
+++ src/RPSC_CARD5.sv
@@ -3,53 +3,55 @@
 module RPSC_CARD5 (i11_Not_G2_OK, i12_Not_DR_AMP_OK, i10_FF3, i9_FF4, i8_FF6, i7_FF2, i6_FF1, 
                     i50_FF1, i51_FF3, i53_Not_G1_OK, i54_Not_DR_AMP_OK, i56_Not_CA_ON, i42_Not_Anode_ON,
 					o16_Not_RF_PERM, o14_FF38, o13_Not_Alarm, o47_FF37, o59_Not_RF_RED);
-	input logic i11_Not_G2_OK, i12_Not_DR_AMP_OK, i10_FF3, i9_FF4, i8_FF6,i7_FF2, i6_FF1;
+	input logic i11_Not_G2_OK, i12_Not_DR_AMP_OK, i10_FF3, i9_FF4, i8_FF6, i7_FF2, i6_FF1;
 					
 	input logic i50_FF1, i51_FF3, i53_Not_G1_OK, i54_Not_DR_AMP_OK, i56_Not_CA_ON, i42_Not_Anode_ON;
 					
-	output logic o16_Not_RF_PERM, o14_RF_perm_b,o13_Not_Alarm, o47_FF37, o59_Not_RF_RED;
+	output logic o16_Not_RF_PERM, o14_FF38, o13_Not_Alarm, o47_FF37, o59_Not_RF_RED;
 	
-	logic RF_perm_b_inv;
-	logic FF_or_out2, nor_out;
+	logic FF1_FF3, nand_out;
 	
-	//Circuit 1 
-	assign o13_Not_Alarm = (i10_FF3 | i9_FF4 | i8_FF6 | i7_FF2 | i6_FF1);
-	assign o16_RF_perm_b = (i11_G2_ok_b | i12_DR_amp_ok_b | o13_Not_Alarm);
-	assign RF_perm_b_inv = ~o16_RF_perm_b;
-	assign o14_FF38 = ~RF_perm_b_inv;
+	//Left Circuit 
+	assign o13_Not_Alarm = ~(i10_FF3 | i9_FF4 | i8_FF6 | i7_FF2 | i6_FF1);
+	assign o16_Not_RF_PERM = ~((~i11_Not_G2_OK) & (~i12_Not_DR_AMP_OK) & o13_Not_Alarm);
+	assign o14_FF38 = o16_Not_RF_PERM;
 	
-	//Circuit 2
-	assign FF_or_out2 = i50_FF1 | i51_FF3;
-	assign nor_out = ~(FF_or_out2 | i53_G1_on_b | i54_DR_amp_on_b | i56_CA_on_b);
-	assign o59_Not_RF_RED = ~(nor_out & i42_Not_Anode_ON);
-							
-	assign o47_FF37 = o59_Not_RF_RED;
+	//Right Circuit
+	assign FF1_FF3 = ~(i50_FF1 | i51_FF3);
+	assign nand_out = ~(FF1_FF3 & (~i53_Not_G1_OK) & (~i54_Not_DR_AMP_OK) & (~i56_Not_CA_ON) & i42_Not_Anode_ON) ;
+	assign o59_Not_RF_RED = nand_out;
+	assign o47_FF37 = nand_out;
 	
 endmodule
 
 module RPSC_CARD5_testbench(); 
   
-  logic i11_Not_G2_OK, i12_Not_DR_AMP_OK, i10_FF3, i9_FF4, i8_FF6, i7_FF2, i6_FF1;
-  logic i50_FF1, i51_FF3, i53_Not_G1_OK, i54_Not_DR_AMP_OK, i56_Not_CA_ON, i42_Not_Anode_ON;
-  logic o16_Not_RF_PERM, o14_FF38, o13_Not_Alarm, o47_FF37, o59_Not_RF_RED; 
-	  
-  RPSC_CARD5 dut (i11_Not_G2_OK, i12_Not_DR_AMP_OK, i10_FF3, i9_FF4, i8_FF6, i7_FF2, i6_FF1, 
-                  i50_FF1, i51_FF3, i53_Not_G1_OK, i54_Not_DR_AMP_OK, i56_Not_CA_ON, i42_Not_Anode_ON,
-		  o16_Not_RF_PERM, o14_FF38, o13_Not_Alarm, o47_FF37, o59_Not_RF_RED);   
-   
-  initial begin   
-   
-    i11_G2_ok_b=0; i12_DR_amp_ok_b=0; i10_FF3=0; i9_FF4=0; i8_FF6=0; i7_FF2=0; i6_FF1=0; 
-    i50_FF1=0; i51_FF3=0; i53_G1_on_b=0; i54_DR_amp_on_b=0; i56_CA_on_b=0; i42_Not_Anode_ON=0; #10;
-    
-    i11_G2_ok_b=0; i12_DR_amp_ok_b=1; i10_FF3=0; i9_FF4=0; i8_FF6=0; i7_FF2=0; i6_FF1=1; 
-    i50_FF1=0; i51_FF3=1; i53_G1_on_b=0; i54_DR_amp_on_b=0; i56_CA_on_b=0; i42_Not_Anode_ON=1; #10;
-    
-    i11_G2_ok_b=1; i12_DR_amp_ok_b=0; i10_FF3=0; i9_FF4=0; i8_FF6=0; i7_FF2=1; i6_FF1=0; 
-    i50_FF1=1; i51_FF3=0; i53_G1_on_b=0; i54_DR_amp_on_b=0; i56_CA_on_b=1; i42_Not_Anode_ON=0; #10;
-    
-    i11_G2_ok_b=1; i12_DR_amp_ok_b=1; i10_FF3=0; i9_FF4=0; i8_FF6=0; i7_FF2=1; i6_FF1=1; 
-    i50_FF1=1; i51_FF3=1; i53_G1_on_b=0; i54_DR_amp_on_b=0; i56_CA_on_b=1; i42_Not_Anode_ON=1; #10;
-	 
-  end   
+	logic i11_Not_G2_OK, i12_Not_DR_AMP_OK, i10_FF3, i9_FF4, i8_FF6, i7_FF2, i6_FF1;
+	logic i50_FF1, i51_FF3, i53_Not_G1_OK, i54_Not_DR_AMP_OK, i56_Not_CA_ON, i42_Not_Anode_ON;
+	logic o16_Not_RF_PERM, o14_FF38, o13_Not_Alarm, o47_FF37, o59_Not_RF_RED; 
+		
+	RPSC_CARD5 dut (.*);   
+
+	integer i;
+
+	initial begin   
+		i = 0; // flip flop are 0
+		{i10_FF3, i9_FF4, i8_FF6, i7_FF2, i6_FF1} = 5'b0;
+		for (int j = 0; j < 4; j++) begin
+			{i11_Not_G2_OK, i12_Not_DR_AMP_OK} = j;
+			#1000;
+		end
+		i = 1; // flip flop are 1
+		{i10_FF3, i9_FF4, i8_FF6, i7_FF2, i6_FF1} = 5'b1;
+		for (int j = 0; j < 4; j++) begin
+			{i11_Not_G2_OK, i12_Not_DR_AMP_OK} = j;
+			#1000;
+		end
+		i = 2; // now test right circuit
+		for (int j = 0; j < 64; j++) begin
+			{i51_FF3, i50_FF1, i56_Not_CA_ON, i54_Not_DR_AMP_OK, i53_Not_G1_OK, i42_Not_Anode_ON} = j;
+			#1000;
+		end
+		$stop;
+	end   
 endmodule 
